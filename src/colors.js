db1cff101d632d291b1456277307fd612811600c
ddelarosae <113042172+ddelarosae@users.noreply.github.com>
2022-11-22 15:13:27 -0500

    1 file modified

--- src/colors.js
+++ src/colors.js
@@ -1,23 +1,26 @@
 const colors = ['red', 'green', 'blue', 'gray', 'yellow'];
 const sizes = ['tiny', 'small', 'medium', 'large', 'huge'];
 const fruits = ['apple', 'banana', 'kiwi', 'pear', 'mango'];
-const items = sizes.reduce((items, size) => [
-  ...items,
-  ...fruits.reduce(
-    (acc, fruit) => [
-      ...acc,
-      ...colors.reduce(
-        (acc, color) => [
-          ...acc,
-          {
-            name: `${size} ${color} ${fruit}`,
-            color,
-          },
-        ],
-        []
-      ),
-    ],
-    []
-  ),
-]);
+const items = sizes.reduce(
+  (items, size) => [
+    ...items,
+    ...fruits.reduce(
+      (acc, fruit) => [
+        ...acc,
+        ...colors.reduce(
+          (acc, color) => [
+            ...acc,
+            {
+              name: `${size} ${color} ${fruit}`,
+              color,
+            },
+          ],
+          []
+        ),
+      ],
+      []
+    ),
+  ],
+  []
+);
 export { items };
